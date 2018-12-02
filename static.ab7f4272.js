c4c86e46c0af24437a233e515e050645996143aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-02 15:16:31 +0000

    Deploy johnie/jobb.johnie.se to github.com/johnie/jobb.johnie.se.git:gh-pages

--- static.ab7f4272.js
+++ static.ab7f4272.js
@@ -67,7 +67,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "https://jobb.johnie.se/";
+/******/ 	__webpack_require__.p = "https://jobb.johniehjelm.me/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 9);
@@ -1681,4 +1681,4 @@ function toComment(sourceMap) {
 /***/ })
 /******/ ]);
 });
-//# sourceMappingURL=static.799716a7.js.map
+//# sourceMappingURL=static.ab7f4272.js.map
