51f15a1cd7cea3250f25e92037929a995f989086
Johnie Hjelm <johnie.hjelm@mrggroup.com>
2018-12-02 16:04:30 +0100

    Fixes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,41 +1,71 @@
 import React from 'react';
 import { withRouteData } from 'react-static';
-import pupa from 'pupa';
 import differenceInYears from 'date-fns/difference_in_years';
 import convert from 'htmr';
 import Button from '../components/button';
 //
 
-const greet = () => {
-	const hours = new Date().getHours();
-	if (hours >= 12) {
-		return hours > 17 ? 'God kväll' : 'God eftermiddag';
-	}
+const pupa = (template, data) => {
+    if (typeof template !== 'string') {
+        throw new TypeError(
+            `Expected a string in the first argument, got ${typeof template}`
+        );
+    }
 
-	return 'Hallå där';
+    if (typeof data !== 'object') {
+        throw new TypeError(
+            `Expected an Object/Array in the second argument, got ${typeof data}`
+        );
+    }
+
+    const regex = /{(.*?)}/g;
+
+    return template.replace(regex, (_, key) => {
+        let ret = data;
+
+        // eslint-disable-next-line
+        for (const prop of key.split('.')) {
+            ret = ret ? ret[prop] : '';
+        }
+
+        return ret || '';
+    });
+};
+
+const greet = () => {
+    const hours = new Date().getHours();
+    if (hours >= 12) {
+        return hours > 17 ? 'God kväll' : 'God eftermiddag';
+    }
+
+    return 'Hallå där';
 };
 
 const getAge = differenceInYears(new Date(), new Date(1992, 4, 8));
 
 export default withRouteData(data => {
-	const title = pupa(data.title.contents, { greeting: greet() });
-	const body = pupa(data.body.contents, { getAge });
-	return (
-		<React.Fragment>
-			<header className="page-title">{convert(title)}</header>
-			<main className="main-content">
-				{convert(body)}
-				<div className="contact-wrap">
-					<Button text="Maila mig" href="mailto:jobb@johnie.se" />
-					<span className="sep">eller</span>
-					<Button
-						bg="var(--third-color)"
-						color="white"
-						text="Boka in mig i kalendern"
-						href="https://calendly.com/johniehjelm/jobb/30min"
-					/>
-				</div>
-			</main>
-		</React.Fragment>
-	);
+    if (typeof document !== 'undefined') {
+        const title = pupa(data.title.contents, { greeting: greet() });
+        const body = pupa(data.body.contents, { getAge });
+        return (
+            <React.Fragment>
+                <header className="page-title">{convert(title)}</header>
+                <main className="main-content">
+                    {convert(body)}
+                    <div className="contact-wrap">
+                        <Button text="Maila mig" href="mailto:jobb@johnie.se" />
+                        <span className="sep">eller</span>
+                        <Button
+                            bg="var(--third-color)"
+                            color="white"
+                            text="Boka in mig i kalendern"
+                            href="https://calendly.com/johniehjelm/jobb/30min"
+                        />
+                    </div>
+                </main>
+            </React.Fragment>
+        );
+    }
+
+    return null;
 });
--- static.config.js
+++ static.config.js
@@ -6,83 +6,82 @@ import chokidar from 'chokidar';
 chokidar.watch('content').on('all', () => reloadRoutes());
 
 export default {
-	preact: true,
-	siteRoot: 'https://jobb.johnie.se',
-	getSiteData: () => ({
-		title: 'Johnie Hjelm',
-		description:
-			'Söker nu en person till mitt härliga team av personliga assistenter!'
-	}),
-	getRoutes: async () => {
-		const { home } = await jdown('content');
-		return [
-			{
-				path: '/',
-				component: 'src/containers/Home',
-				getData: () => ({
-					...home
-				})
-			},
-			{
-				path: '/woho',
-				component: 'src/containers/Woho',
-				getData: () => ({})
-			},
-			{
-				is404: true,
-				component: 'src/containers/404'
-			}
-		];
-	},
-	Document: class CustomHtml extends Component {
-		render() {
-			const { Html, Head, Body, children, renderMeta } = this.props;
+    siteRoot: 'https://jobb.johnie.se',
+    getSiteData: () => ({
+        title: 'Johnie Hjelm',
+        description:
+            'Söker nu en person till mitt härliga team av personliga assistenter!'
+    }),
+    getRoutes: async () => {
+        const { home } = await jdown('content');
+        return [
+            {
+                path: '/',
+                component: 'src/containers/Home',
+                getData: () => ({
+                    ...home
+                })
+            },
+            {
+                path: '/woho',
+                component: 'src/containers/Woho',
+                getData: () => ({})
+            },
+            {
+                is404: true,
+                component: 'src/containers/404'
+            }
+        ];
+    },
+    Document: class CustomHtml extends Component {
+        render() {
+            const { Html, Head, Body, children, renderMeta } = this.props;
 
-			return (
-				<Html>
-					<Head>
-						<meta charSet="UTF-8" />
-						<meta
-							name="msapplication-TileColor"
-							content="#fedf22"
-						/>
-						<meta name="author" content="Johnie Hjelm" />
-						<meta name="theme-color" content="#fedf22" />
-						<meta property="og:type" content="article" />
-						<meta
-							property="og:title"
-							content="Johnie Hjelm söker personlig assistent"
-						/>
-						<meta
-							property="og:url"
-							content="https://jobb.johnie.se"
-						/>
-						<meta
-							property="og:site_name"
-							content="Johnie Hjelm söker personlig assistent"
-						/>
-						<meta
-							property="og:description"
-							content="En driven webbutvecklare som nu söker en person till mitt härliga team av personliga assistenter!"
-						/>
-						<meta name="twitter:site" content="@johniehjelm" />
-						<meta
-							name="viewport"
-							content="width=device-width, initial-scale=1"
-						/>
-						<meta
-							name="description"
-							content="En driven webbutvecklare som nu söker en person till mitt härliga team av personliga assistenter!"
-						/>
-						{renderMeta.styleTags}
-						<script
-							async
-							src="https://www.google-analytics.com/analytics.js"
-						/>
-					</Head>
-					<Body>{children}</Body>
-				</Html>
-			);
-		}
-	}
+            return (
+                <Html>
+                    <Head>
+                        <meta charSet="UTF-8" />
+                        <meta
+                            name="msapplication-TileColor"
+                            content="#fedf22"
+                        />
+                        <meta name="author" content="Johnie Hjelm" />
+                        <meta name="theme-color" content="#fedf22" />
+                        <meta property="og:type" content="article" />
+                        <meta
+                            property="og:title"
+                            content="Johnie Hjelm söker personlig assistent"
+                        />
+                        <meta
+                            property="og:url"
+                            content="https://jobb.johnie.se"
+                        />
+                        <meta
+                            property="og:site_name"
+                            content="Johnie Hjelm söker personlig assistent"
+                        />
+                        <meta
+                            property="og:description"
+                            content="En driven webbutvecklare som nu söker en person till mitt härliga team av personliga assistenter!"
+                        />
+                        <meta name="twitter:site" content="@johniehjelm" />
+                        <meta
+                            name="viewport"
+                            content="width=device-width, initial-scale=1"
+                        />
+                        <meta
+                            name="description"
+                            content="En driven webbutvecklare som nu söker en person till mitt härliga team av personliga assistenter!"
+                        />
+                        {renderMeta.styleTags}
+                        <script
+                            async
+                            src="https://www.google-analytics.com/analytics.js"
+                        />
+                    </Head>
+                    <Body>{children}</Body>
+                </Html>
+            );
+        }
+    }
 };
